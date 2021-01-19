a0c964a949b65b557722de319f4d331d24cdc19e
Tristan Call <tcall@zagmail.gonzaga.edu>
2021-01-18 21:45:30 -0800

    Added basic activity files and some code for them

---
+++ lib.php
@@ -0,0 +1,73 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Library of functions for the distributed_quiz module.
+ *
+ * This contains functions that are called also from outside the distributed_quiz module
+ * Functions that are only called by the distributed_quiz module itself are in {@link locallib.php}
+ *
+ * @package    mod_distributed_quiz
+ * @copyright  1999 onwards Martin Dougiamas {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Given an object containing all the necessary data,
+ * (defined by the form in mod_form.php) this function
+ * will create a new instance and return the id number
+ * of the new instance.
+ *
+ * @param object $distributed_quiz the data that came from the form.
+ * @return mixed the id of the new instance on success,
+ *          false or a string error message on failure.
+ */
+function distributed_quiz_add_instance($distributed_quiz) {
+    global $DB;
+
+
+    return $distributed_quiz->id;
+}
+
+/**
+ * Given an object containing all the necessary data,
+ * (defined by the form in mod_form.php) this function
+ * will update an existing instance with new data.
+ *
+ * @param object $distributed_quiz the data that came from the form.
+ * @return mixed true on success, false or a string error message on failure.
+ */
+function distributed_quiz_update_instance($distributed_quiz) {
+
+    return true;
+}
+
+/**
+ * Given an ID of an instance of this module,
+ * this function will permanently delete the instance
+ * and any data that depends on it.
+ *
+ * @param int $id the id of the distributed_quiz to delete.
+ * @return bool success or failure.
+ */
+function distributed_quiz_delete_instance($id) {
+
+
+    return true;
+}
