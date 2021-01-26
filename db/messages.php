47e4b604a7d9a3f70ea8600b1fde264ef5310287
Tristan Call <tcall@zagmail.gonzaga.edu>
2021-01-25 20:57:58 -0800

    Changed capacity to something that already exists

--- db/messages.php
+++ db/messages.php
@@ -5,6 +5,6 @@ defined('MOODLE_INTERNAL') || die();
 $messageproviders = array (
     // Notify students that a quiz has been created
     'created' => array (
-        'capability'  => 'mod/distributed_quiz:emailnotifycreated'
+        'capability'  => 'mod/quiz:emailconfirmsubmission'
     )
 );
